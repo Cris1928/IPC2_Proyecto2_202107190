3b45114893918cff4e114230e31a0791aa76e5bf
Cris1928 <98928867+Cris1928@users.noreply.github.com>
2022-09-27 19:36:18 -0600

    Delete NodoEscritorio.py

--- NodoEscritorio.py
+++
@@ -1,7 +0,0 @@
-class NodoEscritorio:
-    def __init__(self,id=None,identificacion=None,encargado=None):
-        self.id=id
-        self.identificacion=identificacion
-        self.encargado=encargado
-        self.siguiente=None
-        self.anterior=None
